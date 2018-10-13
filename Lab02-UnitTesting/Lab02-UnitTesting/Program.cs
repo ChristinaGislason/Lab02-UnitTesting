c8357919daf3af4d499c2879be9a7102081f25a8
Christina Gislason <christinamkcho@gmail.com>
2018-10-12 21:18:21 -0700

    created new repo; transferred code from old repo; 5 tests passing

--- Lab02-UnitTesting/Lab02-UnitTesting/Program.cs
+++ Lab02-UnitTesting/Lab02-UnitTesting/Program.cs
@@ -2,11 +2,48 @@
 
 namespace Lab02_UnitTesting
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static decimal Balance = 0;
+
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to your ATM");
         }
+
+        public static decimal ViewBalance()
+        {
+            return Balance;
+        }
+
+        public static bool deposit(decimal amount)
+        {
+            // only deposit if amount is positive
+            if (amount > 0)
+            {
+                Balance += amount;
+                return true;
+            }
+            return false;
+        }
+
+        public static decimal withdraw(decimal amount)
+        {
+            // only withdraw amount if it results in 
+            // balance being greater than zero
+            if (Balance - amount >= 0)
+            {
+                Balance -= amount;
+            }
+            return Balance;
+        }
+        
+
+        // add method to set the balance to anything
+        public static void updateBalance(decimal newBalance)
+        {
+            Balance = newBalance;
+        }
+
     }
 }
